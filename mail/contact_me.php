9574c094306f79c6a6c715f463c74bb1e09fa533
marcterrrence <35656795+marcterrrence@users.noreply.github.com>
2018-06-25 01:35:50 +0700

    Add files via upload

---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+$to = 'prime.bkc@gmail.com'; 
+$email_subject = "Website Contact Form:  $name";
+$email_body = "Prime Website.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
+$headers = "From: noreply@prime.MarcMandagi.com\n"; 
+$headers .= "Reply-To: $email_address";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
